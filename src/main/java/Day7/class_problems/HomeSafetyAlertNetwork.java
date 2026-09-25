79e2e304dcba81cd6637837740a558200ebfc238
shrivallipahade02-spec <shrivallipahade02@gmail.com>
2026-09-25 21:48:40 +0530

    Add Home Safety Alert Network

---
+++ src/main/java/Day7/class_problems/HomeSafetyAlertNetwork.java
@@ -0,0 +1,97 @@
+
+package Day7.class_problems;
+
+interface Alertable {
+    String sendAlert(String message);
+}
+
+class SecuritySensor {
+    private String zoneName;
+
+    public SecuritySensor(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+}
+
+class MotionSensor extends SecuritySensor implements Alertable {
+
+    public MotionSensor(String zoneName) {
+        super(zoneName);
+    }
+
+    @Override
+    public String sendAlert(String message) {
+        return "[" + getZoneName() + "] " + message;
+    }
+}
+
+class DualZoneMotionSensor extends MotionSensor {
+    private String secondZoneName;
+
+    public DualZoneMotionSensor(String zoneName, String secondZoneName) {
+        super(zoneName);
+        this.secondZoneName = secondZoneName;
+    }
+
+    @Override
+    public String sendAlert(String message) {
+        return super.sendAlert(message)
+                + " [also covering " + secondZoneName + "]";
+    }
+}
+
+class SmokeDetector implements Alertable {
+    private String deviceId;
+
+    public SmokeDetector(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    @Override
+    public String sendAlert(String message) {
+        return "[" + deviceId + "] " + message;
+    }
+}
+
+public class HomeSafetyAlertNetwork {
+
+    static void broadcastAll(Alertable[] devices, String message) {
+        for (Alertable device : devices) {
+            System.out.println(device.sendAlert(message));
+        }
+    }
+
+    static String getZoneIfMotionSensor(Alertable a) {
+        if (a instanceof MotionSensor) {
+            MotionSensor sensor = (MotionSensor) a;
+            return sensor.getZoneName();
+        }
+
+        return "Not a motion sensor";
+    }
+
+    public static void main(String[] args) {
+
+        MotionSensor m = new MotionSensor("Living Room");
+
+        DualZoneMotionSensor d =
+                new DualZoneMotionSensor("Hallway", "Stairwell");
+
+        SmokeDetector s = new SmokeDetector("SD-01");
+
+        System.out.println(m.sendAlert("Motion detected"));
+        System.out.println(d.sendAlert("Motion detected"));
+        System.out.println(s.sendAlert("Smoke detected"));
+
+        Alertable[] devices = {m, d, s};
+
+        broadcastAll(devices, "Alert triggered");
+
+        System.out.println(getZoneIfMotionSensor(m));
+        System.out.println(getZoneIfMotionSensor(s));
+    }
+}
